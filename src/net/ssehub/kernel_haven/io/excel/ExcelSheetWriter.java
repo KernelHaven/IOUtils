8dd86e02a23778ef2c2dc945ea7bafcd6d3e1030
Elscha <elscha@sse.uni-hildesheim.de>
2017-10-10 08:43:25 +0200

    Considering Excel cell limit while writing Excel workbooks

--- src/net/ssehub/kernel_haven/io/excel/ExcelSheetWriter.java
+++ src/net/ssehub/kernel_haven/io/excel/ExcelSheetWriter.java
@@ -1,7 +1,10 @@
 package net.ssehub.kernel_haven.io.excel;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.poi.ss.SpreadsheetVersion;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -16,6 +19,8 @@ import net.ssehub.kernel_haven.util.io.AbstractTableWriter;
  *
  */
 public class ExcelSheetWriter extends AbstractTableWriter {
+    
+    private static final int MAX_TEXT_LENGTH = SpreadsheetVersion.EXCEL2007.getMaxTextLength();
     
     private Sheet sheet;
     private int currentRow;
@@ -32,13 +37,48 @@ public class ExcelSheetWriter extends AbstractTableWriter {
 
     @Override
     public void writeRow(String... fields) throws IOException {
-        if (null != fields) {
+        List<String> cellValues = prepareFields(fields);
+        if (null != cellValues) {
             Row row = sheet.createRow(currentRow++);
-            for (int i = 0; i < fields.length; i++) {
+            for (int i = 0; i < cellValues.size(); i++) {
                 Cell cell = row.createCell(i);
-                cell.setCellValue(fields[i]);
+                cell.setCellValue(cellValues.get(i));
             }
         }
+    }
+    
+    /**
+     * Splits text values, which are too long into separate fields to avoid {@link IllegalArgumentException}s.
+     * Tries to splitt values at white space characters.
+     * @param fields The field values of a row to store.
+     * @return The values to write, should be the same values unless there were some values to long.
+     */
+    private List<String> prepareFields(String... fields) {
+        List<String> result = null;
+        if (null != fields) {
+            result = new ArrayList<>();
+            
+            for (int i = 0; i < fields.length; i++) {
+                String fieldValue = fields[i];
+                while (fieldValue.length() > MAX_TEXT_LENGTH) {
+                    String firstPart = fieldValue.substring(0, MAX_TEXT_LENGTH);
+                    
+                    // Try to split at a white space
+                    int pos = firstPart.lastIndexOf(' ');
+                    if (pos == -1) {
+                        pos = MAX_TEXT_LENGTH;
+                    }
+                    
+                    firstPart = fieldValue.substring(0, pos);
+                    result.add(firstPart);
+                    fieldValue = fieldValue.substring(pos);
+                }
+                result.add(fieldValue);
+            }
+            
+        }
+        
+        return result;
     }
 
 }
