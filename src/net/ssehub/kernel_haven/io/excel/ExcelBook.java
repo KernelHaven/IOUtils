1685cf7859374f1c2a7d70e5c2380849c38ed642
Elscha <elscha@sse.uni-hildesheim.de>
2018-02-19 08:53:33 +0100

    Adds new sheets to the end of the document
    Since the bug fix that the output is no longer created in constructor
    rather than the start()-method, the sheets appear in wrong order.

--- src/net/ssehub/kernel_haven/io/excel/ExcelBook.java
+++ src/net/ssehub/kernel_haven/io/excel/ExcelBook.java
@@ -204,12 +204,12 @@ public class ExcelBook implements ITableCollection {
             if (null == sheet) {
                 String cause = null != exception ? ", cause: " + exception.getMessage() : "";
                 throw new IOException("Could not create sheet \"" + safeName + "\"" + cause);
-            } else {
-                /* Add sheets at the front by default
-                 * This is done to show final results at the beginning of document and intermediate results
-                 * at the end of document.
-                 */
-                wb.setSheetOrder(sheet.getSheetName(), 0);
+//            } else {
+//                /* Add sheets at the front by default
+//                 * This is done to show final results at the beginning of document and intermediate results
+//                 * at the end of document.
+//                 */
+//                wb.setSheetOrder(sheet.getSheetName(), 0);
             }
             
             ExcelSheetWriter writer = new ExcelSheetWriter(this, sheet);
